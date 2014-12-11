93c5ff059d1dcf23a346f0c159a68d697f164c32
jalen m moorer <jmm77@afsconnect2.njit.edu>
2014-12-11 09:48:55 -0500

    Added Index Page Router and Autoloader

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('Autoloader.php');
+spl_autoload_register('Autoloader::autoload');
+
+$page = 'home';	
+$Arg = NULL;
+
+if(isset($_REQUEST['page']))
+	{
+		$page = $_REQUEST['page']; 
+			
+	}
+	if(isset($_REQUEST['Arg']))
+	{
+		$Arg = $_REQUEST['Arg'];
+			
+	}
+$class = "Classes\pages\\$page";
+$page = new $class($Arg, $connection);
+
+?>
